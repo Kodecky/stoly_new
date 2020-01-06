fc2cbb3d476934be16566e2246d834364021d972
Damian Pawela <paweladamian@gmail.com>
2020-01-06 22:26:13 +0100

    Add hamburger menu and mobile nav, without nav functions

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
-const toolsBtn = document.querySelector('.tools-icon .icon-btn');
-const calendarBtn = document.querySelector('.calendar-icon .icon-btn');
-const satisfactionBtn = document.querySelector('.handshake-icon .icon-btn');
+// icons section button action
+const toolsBtn = document.querySelector('.tools-icon .icon-btn'),
+    calendarBtn = document.querySelector('.calendar-icon .icon-btn'),
+    satisfactionBtn = document.querySelector('.handshake-icon .icon-btn'),
+    toolsIconCard = document.querySelector('.tools-icon'),
+    toolsCard = document.querySelector('.tools'),
+    calendarIconCard = document.querySelector('.calendar-icon'),
+    calendarCard = document.querySelector('.calendar'),
+    handshakeIconCard = document.querySelector('.handshake-icon'),
+    satisfactionCard = document.querySelector('.satisfaction');
+
 
 toolsBtn.addEventListener('click', () => {
-    document.querySelector('.tools-icon').classList.toggle('hide');
+    toolsIconCard.classList.toggle('hide');
     toolsBtn.classList.toggle('hide');
-    document.querySelector('.tools').classList.toggle('show');
+    toolsCard.classList.toggle('show');
 })
 
 calendarBtn.addEventListener('click', () => {
-    document.querySelector('.calendar-icon').classList.toggle('hide');
+    calendarIconCard.classList.toggle('hide');
     calendarBtn.classList.toggle('hide');
-    document.querySelector('.calendar').classList.toggle('show');
+    calendarCard.classList.toggle('show');
 })
 
 satisfactionBtn.addEventListener('click', () => {
-    document.querySelector('.handshake-icon').classList.toggle('hide');
+    handshakeIconCard.classList.toggle('hide');
     satisfactionBtn.classList.toggle('hide');
-    document.querySelector('.satisfaction').classList.toggle('show');
+    satisfactionCard.classList.toggle('show');
 })
 
-const toolsExitBtn = document.querySelector('.tools i');
-const calendarExitBtn = document.querySelector('.calendar i');
-const satisfactionExitBtn = document.querySelector('.satisfaction i');
+const toolsExitBtn = document.querySelector('.tools i'),
+    calendarExitBtn = document.querySelector('.calendar i'),
+    satisfactionExitBtn = document.querySelector('.satisfaction i');
 
 toolsExitBtn.addEventListener('click', () => {
-    document.querySelector('.tools-icon').classList.toggle('hide');
+    toolsIconCard.classList.toggle('hide');
     toolsBtn.classList.toggle('hide');
-    document.querySelector('.tools').classList.toggle('show');
+    toolsCard.classList.toggle('show');
 })
 
 calendarExitBtn.addEventListener('click', () => {
-    document.querySelector('.calendar-icon').classList.toggle('hide');
+    calendarIconCard.classList.toggle('hide');
     calendarBtn.classList.toggle('hide');
-    document.querySelector('.calendar').classList.toggle('show');
+    calendarCard.classList.toggle('show');
 })
 
 satisfactionExitBtn.addEventListener('click', () => {
-    document.querySelector('.handshake-icon').classList.toggle('hide');
+    handshakeIconCard.classList.toggle('hide');
     satisfactionBtn.classList.toggle('hide');
-    document.querySelector('.satisfaction').classList.toggle('show');
+    satisfactionCard.classList.toggle('show');
+})
+
+// hamburger menu
+const burgerBtn = document.querySelector('.burger-menu'),
+    topLine = document.querySelector('.burger-menu .top-line'),
+    centerLine = document.querySelector('.burger-menu .center-line'),
+    bottomLine = document.querySelector('.burger-menu .bottom-line'),
+    burgerNavCard = document.querySelector('header nav');
+
+burgerBtn.addEventListener('click', () => {
+    topLine.classList.toggle('backslash');
+    centerLine.classList.toggle('no-width');
+    bottomLine.classList.toggle('forward-slash');
+    burgerNavCard.classList.toggle('hide-nav');
 })
